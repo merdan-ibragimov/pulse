6dae8a4fbd1f41794ca7e9fbca570efb7a734925
Merdan <merdan1ibragimov@gmail.com>
2021-01-31 00:42:28 +0200

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,15 @@
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png"></img></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png"></img></button>',
+        responsive:[
+          {
+            breakpoint: 992,
+            settings: {
+              arrows: false
+            }
+          }
+        ]
     });
   });
           
